e636be9e2f4b196390dc581913de5e1488eb2625
Fritz Dittmer <fdittmer9762@gmail.com>
2017-08-25 14:02:31 -0600

    Class Input Demo
    worked with key inputs and action events

---
+++ Scripting2670/Assets/Scripts/MovementInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MovementInput : MonoBehaviour {
+
+    Action KeyAction;                                           //action used to move character controller
+
+    private void Start()
+    {
+        KeyAction = Move;                                       //Assigns Move to Keyaction so when something calls for the keyaction event it will run the Move method
+    }
+
+    void Update () {
+        MoveInput();                                            //runs Move Input Every Frame
+    }
+
+    void MoveInput() {                                          //checks for input
+        if (Input.GetKey(KeyCode.LeftArrow))                    //Tests for input from left arrow key
+        {
+            KeyAction();                                        //Calls action KeyAction when 
+        }
+    }
+
+    private void Move()
+    {
+        Debug.Log("Left Arrow");                                //for testing
+    }
+}
