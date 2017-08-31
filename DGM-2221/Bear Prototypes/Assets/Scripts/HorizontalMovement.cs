b6cce5f61b62629c91fe64505f4cdeb338983772
Fritz Dittmer <fdittmer9762@gmail.com>
2017-08-31 11:18:12 -0600

    2221 Bear Prototype
    Started Project
    Saved Prototype Scene
    Created Player
    Horizontal Movement Added

---
+++ DGM-2221/Bear Prototypes/Assets/Scripts/HorizontalMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMovement : MonoBehaviour
+{
+
+    public CharacterController playerCC;
+    float speed = 5;
+    Vector3 tempPos;
+
+    void Start()
+    {
+        playerCC = GetComponent<CharacterController>();
+        PlayerMoveInput.HorizontalInput += Movement;
+    }
+
+    void Movement(float obj)
+    {
+        tempPos.x = obj * speed * Time.deltaTime;
+        playerCC.Move(tempPos);
+        print(obj);
+    }
+
+}
---
+++ DGM-2221/Bear Prototypes/Assets/Scripts/PlayerMoveInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerMoveInput : MonoBehaviour {
+
+    public static Action<float> HorizontalInput;
+	
+	void Update () {
+        MoveInput();
+	}
+
+    void MoveInput() {
+        if (HorizontalInput != null) {
+            HorizontalInput(Input.GetAxis("Horizontal"));
+        }
+    }
+}
