f9f3aaf8b10c1b93bb5f7530faa885ec2a54639d
Fritz Dittmer <fdittmer9762@gmail.com>
2017-09-08 10:14:43 -0600

    Cleaned up falling tree script
    added notation
    added proper acceleration to tree falling

--- DGM-2221/Bear Prototypes/Assets/Scripts/FallingTree.cs
+++ DGM-2221/Bear Prototypes/Assets/Scripts/FallingTree.cs
@@ -5,41 +5,41 @@ using UnityEngine;
 public class FallingTree : MonoBehaviour {
 
     public float felledAngle;                                                   //publicly difined float that holds the angle that the tree will fall after pushed
-    private float fallSpeed = 30f;
-    protected float fallTime;
-    float percentFallen;
+    private float fallSpeed = 2f;                                               //the rate of acceleration for the falling (gravity) **still testing**
+    protected float fallTime;                                                   //time to fall, calculated using felledAngle and fallSpeed
+    float percentFallen;                                                        //used for lerping
 
-    Vector3 rotValue;
-    Quaternion myRotate;
+    Vector3 rotValue;                                                           //temp var used to set transform
+    Quaternion myRotate;                                                        //quaterion used to set the transform.rotate values
 
     void OnTriggerEnter() {
-        print("press c to push over");
-        ControlManager.ActivateAction += Timber;
+        //print("press c to push over");
+        ControlManager.ActivateAction += Timber;                                //Subs to Activate Action
     }
 
     void OnTriggerExit() {
-        print("well you can't knock over a tree from over there now can you.");
-        ControlManager.ActivateAction -= Timber;
+        //print("well you can't knock over a tree from over there now can you.");
+        ControlManager.ActivateAction -= Timber;                                //unSubs to activate action
     }
 
     void Timber() {
-        StartCoroutine(TreeFalls());
+        StartCoroutine(TreeFalls());                                            //Starts Coroutine to make tree fall
     }
 
     IEnumerator TreeFalls () {
-        fallTime = felledAngle / fallSpeed;
-        if (felledAngle != transform.rotation.z) {
-            while (rotValue.z < felledAngle) {
-                percentFallen += (1 / fallTime) * Time.deltaTime;
-                print("current angle: " + rotValue.z);
-                rotValue.z = Mathf.Lerp(0, felledAngle, percentFallen);
-                myRotate.z = rotValue.z;
+        fallTime = felledAngle / fallSpeed;                                     //time = distance/velocity
+        float tempAngle = 0;                                                    //float used for acceleration
+            while (rotValue.z < felledAngle) {                                  //while you have not reached your destination, keep falling
+                tempAngle += (1 / fallTime) * Time.deltaTime;                   //calculates the rate of acceleration and adjusts for framerate
+                percentFallen += tempAngle;                                     //applys acceleration to the velocity
+                rotValue.z = Mathf.Lerp(0, felledAngle, percentFallen);         //calculates the angle the tree needs to be at
+                myRotate.z = rotValue.z;                                        
+                print("current angle: " + myRotate.z);                          //***for debugging***
                 transform.rotation = myRotate;// ********this is where the issue is!!!!!!!!*********
                 //*****Having issues getting and setting correct values from transform.rotation*****
-                print("Actual angle: " + transform.rotation.z);
-                yield return null;
+                print("Actual angle: " + transform.rotation.z);                 //*debug*
+                yield return null;                                              //waits unitil the next frame to loop through again
             }
-        }
-        print("Crash!!!!!");
+        print("Crash!!!!!");                                                    //*debug*
     }
 }
