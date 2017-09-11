8f9b80e0b3d993534a698e30bb27c1f21efe80cc
Fritz Dittmer <fdittmer9762@gmail.com>
2017-09-11 11:44:00 -0600

    Inventory / Collectable Object
    created a collectable object
    **needs to disable itself when collected**
    Added inventory to keep track of Items
    Inventory changes adds or subtracks when item is collected
    Cleaned up Project **removed unused assets**

---
+++ DGM-2221/Bear Prototypes/Assets/Scripts/CollectObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CollectObject : MonoBehaviour {
+
+    public static Action<int> CollectAction;
+    public int itemType;
+    private GameObject currentGO;
+
+    void Start() {
+        //currentGO = GetComponent<GameObject>();
+        //if (currentGO != null) { print("done got the object"); }
+    }
+
+    void OnTriggerEnter() {
+        OnCollectAction();
+    }
+
+    void OnCollectAction() {
+        if (CollectAction != null) {
+            CollectAction(itemType);
+        }
+        //print(itemType + " :Collected");
+        //currentGO.gameObject.SetActive(false); **bugs fix later**
+        //disable Item (make it uncollectable)
+    }
+}
---
+++ DGM-2221/Bear Prototypes/Assets/Scripts/Inventory.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    protected int berries, fish, rocks;
+
+	void Start () {
+        CollectObject.CollectAction += AddToInventory;
+	}
+
+    void AddToInventory(int Item) {
+        switch (Mathf.Abs(Item)) {
+            case 1:
+                berries += AddOrSub(Item);
+                break;
+            case 2:
+                fish += AddOrSub(Item);
+                break;
+            case 3:
+                rocks += AddOrSub(Item);
+                break;
+            default:
+                print("you done did goofed, I cant add this");
+                break;
+        }
+        print("Current Stash: Berries: " + berries + ", Fish: " + fish + ", Rocks: " + rocks);
+    }
+
+    int AddOrSub(int obj) {
+        if (obj != 0) {
+            if (obj > 0) { return 1; }
+            if (obj < 0) { return -1; }
+        }
+        return 0;
+    }
+}
