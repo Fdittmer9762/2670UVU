d6f4cb26d2614207043e7149c46b6df59ec24892
Fritz Dittmer <fdittmer9762@gmail.com>
2017-09-20 15:01:27 -0600

    Bear Agro Area
    Added Bear Agro Area
    Created List of targets for the bear
    Objects Add themselves to the list when they enter the agro area

---
+++ DGM-2221/Bear Prototypes/Assets/Scripts/AgroObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AgroObject : MonoBehaviour {
+
+    public static Action<GameObject, int> AddAgroObject;
+    public int tastyScale = 0;
+
+    void OnTriggerEnter() {
+        if (AddAgroObject != null) { AddAgroObject(this.gameObject,tastyScale); }
+    }
+
+}
---
+++ DGM-2221/Bear Prototypes/Assets/Scripts/BearChase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearChase : MonoBehaviour {
+
+    public List<GameObject> Targets;
+    
+
+    void Start () {
+        AgroObject.AddAgroObject += OnNewTarget;
+    }
+
+    void OnNewTarget(GameObject newTarget, int tS) {
+        if(Targets.Count > 0) {
+            for (int i = 0; i < Targets.Count; i++) {
+                print(newTarget.name + " is only " +FindDistance(this.gameObject, newTarget)+ " meters away!");
+            }
+        }
+        else { Targets.Add(newTarget); print(newTarget.name+ " is the only thing I see"); }
+        
+        FindDistance(newTarget, this.gameObject);
+    }
+
+    float FindDistance(GameObject objA, GameObject objB){
+        return Vector3.Distance(objA.transform.position, objB.transform.position);
+    }
+
+    float FindDistance(Transform objA, Transform objB) {
+        return Vector3.Distance(objA.position, objB.position);
+    }
+}
