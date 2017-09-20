662fbdcae504d00e346b3d3ee2a96352701275a7
Fritz Dittmer <fdittmer9762@gmail.com>
2017-09-20 11:50:34 -0600

    Bear Prototype Setup
    created agro area
    created agro object event
    **figuring out lists**

---
+++ DGM-2221/Bear Prototypes/Assets/Scripts/AddAgroObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AddAgroObject : MonoBehaviour {
+
+    public static Action<GameObject, int> AddAgroObjectAction;
+    public int tastyScale;
+
+    void OnTriggerEnter() {
+        print(tastyScale);
+        if (AddAgroObjectAction != null) { AddAgroObjectAction(this.gameObject, 0); }
+    }
+}
