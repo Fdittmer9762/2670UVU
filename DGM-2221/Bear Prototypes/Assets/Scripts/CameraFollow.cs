b9912a0467ea7278e98b4e40e361b95f0ddea567
Fritz Dittmer <fdittmer9762@gmail.com>
2017-09-25 17:49:03 -0600

    Camera Movement Added
    added triggers and an event system that move the camera

--- DGM-2221/Bear Prototypes/Assets/Scripts/CameraFollow.cs
+++ DGM-2221/Bear Prototypes/Assets/Scripts/CameraFollow.cs
@@ -7,16 +7,29 @@ public class CameraFollow : MonoBehaviour {
     float smoothSpeed = 0.125f;
     public Vector3 cameraOffset;
 
-	// Use this for initialization
 	void Start () {
         PlayerMovement.PlayerLocationAction += FollowTarget;
+        ChangeCameraOffset.ChangeCameraOffsetAction += OnNewCameraPos;
 	}
 
     void FollowTarget(Vector3 targetPos) {
         Vector3 desiredPos = targetPos + cameraOffset;
         Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
         transform.position = smoothedPos;
-        //print(targetPos.x + ", " + targetPos.y + ", " + targetPos.z);
         transform.LookAt(targetPos);
+    }
+
+    void OnNewCameraPos(Vector3 newOffset, float camSpeed) {
+        StartCoroutine(UpdateCameraOffset(cameraOffset,newOffset, camSpeed));
+    }
+
+    IEnumerator UpdateCameraOffset(Vector3 currentOffset, Vector3 newOffset, float camSpeed) {
+        float travelDist = 0f;
+        while (travelDist <= 1f) {
+            travelDist += Time.deltaTime* camSpeed;
+            cameraOffset = Vector3.Lerp(currentOffset, newOffset, travelDist);
+            print("Camera reorientating" + travelDist);
+            yield return null;
+        }
     }
 }
---
+++ DGM-2221/Bear Prototypes/Assets/Scripts/ChangeCameraOffset.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ChangeCameraOffset : MonoBehaviour {
+
+    public static Action<Vector3, float> ChangeCameraOffsetAction;
+
+    public Vector3 newCameraOffset;
+    public float cameraSpeed = .5f;
+
+    void OnTriggerEnter() {
+        if (ChangeCameraOffsetAction != null) { ChangeCameraOffsetAction(newCameraOffset, cameraSpeed); }
+    }
+
+}
