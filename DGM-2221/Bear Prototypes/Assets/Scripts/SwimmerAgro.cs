0d343d009d92ae06108ff8ec6b6c3b3bcb5f56f3
Fritz Dittmer <fdittmer9762@gmail.com>
2017-09-26 14:58:08 -0600

    Created Swimming NPCs
    created swimming things (fish, leaches, etc)
    **issues staying in water**
    follow the player when the player gets in range
    **issues with player detection when multiple are persuing the player**

---
+++ DGM-2221/Bear Prototypes/Assets/Scripts/SwimmerAgro.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SwimmerAgro : MonoBehaviour {
+
+    public static Action PlayerDetectionAction;
+    public static Action PlayerEscapeAction;
+
+    void OnTriggerEnter() {
+        print("playerfound");
+        if (PlayerDetectionAction != null) { PlayerDetectionAction(); }
+    }
+
+    void OnTriggerExit() {
+        print("playerEscaped");
+        //if (PlayerEscapeAction != null) { PlayerEscapeAction(); }
+    }
+
+}
---
+++ DGM-2221/Bear Prototypes/Assets/Scripts/WaterFollow.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class WaterFollow : MonoBehaviour {
+
+    private float maxAgroDistance = 20f;
+    public GameObject swimmer;
+    private bool swimmerInWater;
+    public float swimmerSpeed;
+    private bool playerChaseable = false;
+
+    void Start() {
+        SwimmerAgro.PlayerDetectionAction += OnPlayerDetect;
+        //swimmer = GetComponent<GameObject>();
+    }
+
+    void OnPlayerDetect() {
+        SwimmerAgro.PlayerDetectionAction -= OnPlayerDetect;
+        SwimmerAgro.PlayerEscapeAction += OnPlayerEscape;
+        PlayerMovement.PlayerLocationAction += CompareDistance;
+    }
+
+    void OnPlayerEscape() {
+        SwimmerAgro.PlayerEscapeAction -= OnPlayerEscape;
+        SwimmerAgro.PlayerDetectionAction += OnPlayerDetect;
+        if (playerChaseable ) { PlayerMovement.PlayerLocationAction -= FollowPlayer; }
+        playerChaseable = false;
+    }
+
+    void CompareDistance(Vector3 _targetPos) {
+        if (Vector3.Distance(this.gameObject.transform.position, _targetPos) <= maxAgroDistance) {
+            PlayerMovement.PlayerLocationAction += FollowPlayer;
+            playerChaseable = true;
+        }
+        else { /*nothing*/ }
+        PlayerMovement.PlayerLocationAction -= CompareDistance;
+    }
+
+    void FollowPlayer(Vector3 _targetPos) {
+        swimmer.transform.LookAt(_targetPos);
+        //translate forward
+        if (swimmerInWater) { swimmer.transform.Translate(Vector3.forward* Time.deltaTime* swimmerSpeed); }
+    }
+
+    void OnTriggerEnter() {
+        print("Water Entered");
+        swimmerInWater = true;
+    }
+
+    void OnTriggerExit() {
+        print("WaterExited");
+        swimmerInWater = false;
+    }
+}
