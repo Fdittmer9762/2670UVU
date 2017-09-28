2f33a3f8edc24b4032fb7e9b082398ea91effa37
Fritz Dittmer <fdittmer9762@gmail.com>
2017-09-27 20:00:37 -0600

    Doll House Door Feature Added
    Added a dollhouse door mechanic that removes objects that would obscure the players view of the player character (walls, set dressing, etc) for seemless transitions to indoor or underground areas (tunnes, etc)

---
+++ DGM-2221/Bear Prototypes/Assets/Scripts/DollHouse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollHouse : MonoBehaviour {
+
+    public GameObject dollHouseDoor;                                    //gameobject that obscures the area from view (manually set)
+
+	void Start () {
+        //if (dollHouseDoor = null) { dollHouseDoor = GetComponentInChildren<GameObject>(); } // does not work
+        //dollHouseDoor = GetComponentInChildren<GameObject>();
+        //**make sure that the door is a child object of the area trigger
+    }
+
+    void OnTriggerEnter() {                                             //when the player enters the area
+        ChangeDollHouse(false);                                         //will open the dollhouse door
+    }
+
+    void OnTriggerExit() {                                              //when the player exits the area
+        ChangeDollHouse(true);                                          //will close the dollhouse door
+    }
+
+    void ChangeDollHouse(bool _shouldclose) {                           // activates or deactivates the gameobject that would obscure an area(dollhouse door)
+        dollHouseDoor.SetActive(_shouldclose);                          //true closes door(player cannot see inside) false opens door (player can see inside)
+        //**refine for later, create a disolve effect**
+    }
+}
