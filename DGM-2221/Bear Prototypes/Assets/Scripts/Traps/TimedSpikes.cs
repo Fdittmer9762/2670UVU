76776066bb7538e6246608118847fc040453b196
Fritz Dittmer <fdittmer9762@gmail.com>
2017-09-29 14:59:31 -0600

    Timed Spikes
    Created the scripts to move the timed spikes
    created an event system that will activate any subed traps/devices

---
+++ DGM-2221/Bear Prototypes/Assets/Scripts/Traps/TimedSpikes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedSpikes : MonoBehaviour {
+
+    private float offset, cycleTime = 2f;
+    private bool trapActive = false;
+
+    void Start() {
+        TrapManager.ActivateTrapsAction += OnActivateAction;
+    }
+
+    void OnActivateAction(bool _activate) {
+        trapActive = _activate;
+        if (trapActive) { StartCoroutine(CycleSpikes()); }
+    }
+
+    IEnumerator CycleSpikes() {
+        yield return new WaitForSeconds(offset);
+        while (trapActive) {
+            yield return new WaitForSeconds(cycleTime);
+            //fire animation that shoots up the spikes
+            print("Stab");
+        }
+    }
+}
---
+++ DGM-2221/Bear Prototypes/Assets/Scripts/Traps/TrapManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TrapManager : MonoBehaviour {
+
+    public static Action<bool> ActivateTrapsAction;
+
+    void OnTriggerEnter() {
+        if (ActivateTrapsAction != null) { ActivateTrapsAction(true); }
+    }
+
+    void OnTriggerExit() {
+        if (ActivateTrapsAction != null) { ActivateTrapsAction(false); }
+    }
+}
