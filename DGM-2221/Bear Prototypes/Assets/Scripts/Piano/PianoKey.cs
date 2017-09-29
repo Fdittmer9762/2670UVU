7f8d2a1f36d945fced9e5b3b5c1aacbaff978cce
Fritz Dittmer <fdittmer9762@gmail.com>
2017-09-29 16:47:13 -0600

    Piano Puzzle
    Added and debugged Piano puzzle script

---
+++ DGM-2221/Bear Prototypes/Assets/Scripts/Piano/PianoKey.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[RequireComponent(typeof(AudioSource))]
+public class PianoKey : MonoBehaviour {
+
+    public static Action<int> PlayNoteAction;
+
+    public int pitchOffset; //1 = C, 12 = root
+    //enum Pitches {C,Dflat,D,Eflat,E,Fflat,F,Gflat,G,Aflat,A,Bflat,B,Root }
+    private AudioSource _audio;
+    //Pitches pitch;
+
+    void Start() {
+        _audio = GetComponent<AudioSource>();
+    }
+
+
+    void OnTriggerEnter() {
+        PlayNote();
+    }
+
+    void PlayNote() {
+        _audio.pitch = Mathf.Pow(2f, pitchOffset / 12.0f);
+        _audio.PlayDelayed(0);
+        if (PlayNoteAction != null) { PlayNoteAction(pitchOffset); }
+    }
+
+}
---
+++ DGM-2221/Bear Prototypes/Assets/Scripts/Piano/PianoManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoManager : MonoBehaviour {
+
+    public int[] answerKey;
+    private int step = 0;
+
+    void OnTriggerEnter() {
+        PianoKey.PlayNoteAction += OnNotePlayed;
+    }
+    void OnTriggerExit() {
+        if (PianoKey.PlayNoteAction != null) { PianoKey.PlayNoteAction -= OnNotePlayed; }
+    }
+
+    void OnNotePlayed(int note) {
+        if (note == answerKey[step]) { step++; print(note + " is correct"); }
+        else { step = 0; print("Wrong Note " + note + " is not the same as " + answerKey[step]); }
+        if (step >= answerKey.Length) { step = 0; PianoKey.PlayNoteAction -= OnNotePlayed; print("Success"); }
+    }
+
+}
