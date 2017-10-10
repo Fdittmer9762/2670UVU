90712b2b757a236bb6da2539a83335695cf79aaa
Fritz Dittmer <fdittmer9762@gmail.com>
2017-10-10 16:40:01 -0600

    Added Camera Targeting
    added script that passes new targets to a list on the camera (removes as well) 
    added functionality that allows the camera to lerp to the average of all targets within the the list (follow all targets)

--- DGM-2221/Bear Prototypes/Assets/Scripts/CameraFollow.cs
+++ DGM-2221/Bear Prototypes/Assets/Scripts/CameraFollow.cs
@@ -6,13 +6,21 @@ public class CameraFollow : MonoBehaviour {
 
     float smoothSpeed = 0.125f;
     public Vector3 cameraOffset;
+    private Vector3 averageTargetPos, targetPos;
+    public List<Transform> cameraTargets;
+    public Transform playerTransform; //set in inspector
+    private bool camLerping = false;
 
 	void Start () {
         PlayerMovement.PlayerLocationAction += FollowTarget;
         ChangeCameraOffset.ChangeCameraOffsetAction += OnNewCameraPos;
+        CamTargetScript.NewCamTargetAction += OnNewTarget;
+        cameraTargets = new List<Transform>();
+        cameraTargets.Add(playerTransform);
 	}
 
-    void FollowTarget(Vector3 targetPos) {
+    void FollowTarget(Vector3 playerPos) { //avoid using targetPos
+        if (!camLerping) { targetPos = FindAverage(); }
         Vector3 desiredPos = targetPos + cameraOffset;
         Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
         transform.position = smoothedPos;
@@ -31,5 +39,47 @@ public class CameraFollow : MonoBehaviour {
             //print("Camera reorientating" + travelDist);
             yield return null;
         }
+    }
+
+    void OnNewTarget(Transform _newTarget, bool shouldAdd, float _camSpeed) {
+        StartCoroutine(NewCameraTarget(_newTarget, shouldAdd, _camSpeed));
+    }
+
+    IEnumerator NewCameraTarget(Transform _target, bool shouldAdd, float _camSpeed) {
+        Vector3 oldPos = FindAverage();
+        if (shouldAdd) {
+            cameraTargets.Add(_target);
+        }else{ RemoveTarget(_target); }
+        camLerping = true;
+        float travelDist = 0f;
+        while (travelDist < 1f)
+            {
+            travelDist += Time.deltaTime * (1 / _camSpeed);
+            targetPos = Vector3.Lerp(cameraTargets[0].position, FindAverage(), travelDist); //clean this shit up!!
+            if (cameraTargets.Count <= 1) { targetPos = Vector3.Lerp(oldPos,cameraTargets[0].position, travelDist); }
+            yield return null;
+            }
+            camLerping = false;
+    }
+
+    Vector3 FindAverage() {
+        averageTargetPos = Vector3.zero;
+        for (int i = 0; i < cameraTargets.Count; i++) {
+            averageTargetPos += cameraTargets[i].position;
+        }
+        averageTargetPos /= cameraTargets.Count;
+        return averageTargetPos;
+    }
+
+    void RemoveTarget(Transform targetGO) {
+        if (cameraTargets.Count > 0){
+            for (int i = 0; i < cameraTargets.Count; i++){
+                if (targetGO == cameraTargets[i]){
+                    cameraTargets.RemoveAt(i);
+                    print(targetGO.name + " has been Removed!");
+                }
+            }
+        }
+        //remove target from list
     }
 }
---
+++ DGM-2221/Bear Prototypes/Assets/Scripts/CameraScripts/CamTargetScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CamTargetScript : MonoBehaviour {
+
+    public static Action<Transform, bool, float> NewCamTargetAction;
+
+    public Transform newCamTarget;                                                      //set in inspector, holds transform of new camera target
+    public float cameraMoveTime;
+
+    void ChangeCameraTarget(bool addTarget) {
+        if (NewCamTargetAction != null) { NewCamTargetAction(newCamTarget, addTarget, cameraMoveTime); }
+    }
+
+    void OnTriggerEnter() {
+        print(newCamTarget.position.x + " Has been sent");
+        ChangeCameraTarget(true);
+    }
+    void OnTriggerExit() {
+        ChangeCameraTarget(false);
+    }
+}
