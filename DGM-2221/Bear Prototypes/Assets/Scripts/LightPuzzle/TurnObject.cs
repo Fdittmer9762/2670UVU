bb7f497106f316d45545528a3d061826d648e2bf
Fritz Dittmer <fdittmer9762@gmail.com>
2017-10-12 18:02:46 -0600

    MirrorPuzzle
    updated the mirror puzzle, 
    allowed for controls

---
+++ DGM-2221/Bear Prototypes/Assets/Scripts/LightPuzzle/TurnObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnObject : MonoBehaviour {
+
+    private Transform TurnableObject;
+    private float rotSpeed = 15f;
+
+    void Start() {
+        TurnableObject = this.transform.GetChild(0).transform;
+    }
+
+    void OnTriggerEnter() {
+        PlayerMoveInput.VerticalInputAction += RotateObject;
+    }
+
+    void RotateObject(float _rotValue) {
+        TurnableObject.transform.Rotate( Vector3.forward * Time.deltaTime *_rotValue *rotSpeed);
+    }
+
+}
